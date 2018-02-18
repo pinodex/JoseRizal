639bb93042a047e74e772c25706d26bb2ce0f288
Raphael Marco <raphaelmarco@outlook.com>
2018-02-18 13:24:49 +0800

    Fix missing overlay style

--- src/pages/Works/Work.vue
+++ src/pages/Works/Work.vue
@@ -106,4 +106,14 @@
   .content-container {
     margin-top: 2rem;
   }
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    background: rgba(0, 0, 0, 0.5);
+  }
 </style>
